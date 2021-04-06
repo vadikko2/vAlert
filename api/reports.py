d1b981be01e81008d2d7256e622ecc333528c934
Vadim Kozyrevskii <Vadim.Kozyrevskii@kronshtadt.ru>
2021-04-06 13:32:41 +0300

    add stupit login method

--- api/reports.py
+++ api/reports.py
@@ -1,9 +1,9 @@
 from typing import Optional
 from uuid import UUID
 
-from fastapi import APIRouter, Request, Cookie, Response
+from fastapi import APIRouter, Request, Cookie
 
-from api.models import ReportCreated, Report, Resolver
+from api.models import ReportCreated, Report
 from broker.publisher import publish
 
 router = APIRouter(
@@ -12,19 +12,18 @@ router = APIRouter(
 
 
 @router.post('/create', response_model=ReportCreated)
-async def create_report(report: Report, request: Request, response: Response):
+async def create_report(report: Report, request: Request):
     uuid = report.id
     departure = report.departure
     critical = report.critical
     await publish(request.app.extra['broker_connection'], str(uuid), departure, critical)
-    response.set_cookie(key='user_uuid', value=str(uuid))  # todo это просот пример того, как в FastAPI работает cookie
     return ReportCreated(created_datetime=report.date, uuid=uuid)
 
 
-@router.get('/get', response_model=UUID)
+@router.get('/get', response_model=bool)  # todo должен возвращать Report
 async def get_report(user_uuid: Optional[UUID] = Cookie(None)):
     # todo обращаемся к раббиту и по полученной заявке обновляем в БД поле resolver
     if not user_uuid:
-        return None
+        return False
     else:
-        return user_uuid
+        return True  # todo тут надо вернуть ральный Report, полученный из базы по uuid из раббита
--- api/users.py
+++ api/users.py
@@ -1,4 +1,6 @@
-from fastapi import APIRouter
+from uuid import uuid4, UUID
+
+from fastapi import APIRouter, Response
 
 router = APIRouter(
     prefix='/users'
@@ -8,3 +10,11 @@ router = APIRouter(
 @router.get('/{user}')
 async def get_user(user: str):
     return user
+
+
+@router.get('/login', response_model=UUID)
+async def login(response: Response):
+    # todo супер тупая заглука аутентификации тупо для того, что бы ну хоть что-то было в куки
+    uuid = uuid4()
+    response.set_cookie(key='user_uuid', value=str(uuid))
+    return uuid
