bf05ed4c08eea6094955da70089f05829d05972a
Vadim Kozyrevskii <Vadim.Kozyrevskii@kronshtadt.ru>
2021-04-27 09:22:36 +0300

    update intial migration

--- db/alembic/versions/c2f98a9d6db4_init.py
+++ db/alembic/versions/c2f98a9d6db4_init.py
@@ -8,8 +8,16 @@ Create Date: 2021-04-26 17:12:43.428716
 """
 from alembic import op
 
-from sqlalchemy import Text, Float, Date, ForeignKey, Boolean, Column
-from sqlalchemy.dialects.postgresql import UUID
+import sys
+
+sys.path.append('../')
+
+from vAlert.database.models import (
+    ReportsModel,
+    CriticalTypesModel,
+    RolesModel, SubdivisionsModel,
+    PositionsModel, UsersModel, ReportStatusModel
+)
 
 revision = 'c2f98a9d6db4'
 down_revision = None
@@ -19,81 +27,36 @@ depends_on = None
 
 def upgrade():
     op.create_table(
-        'critical_types',
-        Column('id', UUID(as_uuid=True), primary_key=True, comment='Идентификатор'),
-        Column('name', Text, comment='Название типа критичности сообщения'),
-        Column('frequency', Float, comment='Вероятность выдачи заяки'),
-        schema='public', comment='Список типов критичности заявок'
+        CriticalTypesModel.__tablename__, *CriticalTypesModel.__schema__
+    )
+    op.create_table(
+        RolesModel.__tablename__, *RolesModel.__schema__
     )
 
     op.create_table(
-        'roles',
-        Column('id', UUID(as_uuid=True), primary_key=True, comment='Идентификатор'),
-        Column('name', Text, unique=True, comment='Название роли'),
-        schema='public', comment='Список ролей'
+        SubdivisionsModel.__tablename__, *SubdivisionsModel.__schema__
     )
 
     op.create_table(
-        'subdivision',
-        Column('id', UUID(as_uuid=True), primary_key=True, comment='Идентификатор'),
-        Column('name', Text, unique=True, comment='Наименование подразделения'),
-        Column('parent_subdivision', UUID(as_uuid=True), ForeignKey('public.subdivision.id'),
-               comment='Идентификатор родительского подразделения'),
-        schema='public', comment='Список подразделений'
+        PositionsModel.__tablename__, *PositionsModel.__schema__
     )
 
     op.create_table(
-        'positions',
-        Column('id', UUID(as_uuid=True), primary_key=True, comment='Идентификатор'),
-        Column('name', Text, unique=True, comment='Наименование позиции'),
-        Column('subdivision', UUID(as_uuid=True), ForeignKey('public.subdivision.id'),
-               comment='Идентификатор подразделения'),
-        Column('parent_position', UUID(as_uuid=True), ForeignKey('public.positions.id'),
-               comment='Идентификатор руководителя'),
-        schema='public', comment='Список должностей'
+        UsersModel.__tablename__, *UsersModel.__schema__
     )
 
     op.create_table(
-        'users',
-        Column('id', UUID(as_uuid=True), primary_key=True, comment='Идентификатор'),
-        Column('first_name', Text, comment='Имя'),
-        Column('last_name', Text, comment='Фамилия'),
-        Column('patronymic', Text, comment='Отчество'),
-        Column('email', Text, unique=True, comment='Адрес электронной почты'),
-        Column('password_hash', Text, comment='Хэш от пароля'),
-        Column('role', UUID(as_uuid=True), ForeignKey('public.roles.id'), comment='Идентификатор роли'),
-        Column('disabled', Boolean, comment='Активный пользователь'),
-        Column('position', UUID(as_uuid=True), ForeignKey('public.positions.id'), comment='Идентификатор должности'),
-        schema='public', comment='Список пользователей'
+        ReportStatusModel.__tablename__, *ReportStatusModel.__schema__
     )
 
-    op.create_table(
-        'report_status',
-        Column('id', UUID(as_uuid=True), primary_key=True, comment='Идентификатор'),
-        Column('name', Text, unique=True, comment='Наименование'),
-        schema='public', comment='Список статусов заявки'
-    )
-    op.create_table(
-        'reports',
-        Column('id', UUID(as_uuid=True), primary_key=True, comment='Идентификатор'),
-        Column('title', Text, comment='Название заявки'),
-        Column('description', Text, comment='Описание заявки'),
-        Column('timestamp', Date, comment='Времянная метка'),
-        Column('reporter', UUID(as_uuid=True), ForeignKey('public.users.id'),
-               comment='Идентификатор создателя заявки'),
-        Column('critical_types', UUID(as_uuid=True), ForeignKey('public.critical_types.id'), comment='Критичность'),
-        Column('resolver', UUID(as_uuid=True), ForeignKey('public.users.id'), comment='Идентификатор эксперта'),
-        Column('status', UUID(as_uuid=True), ForeignKey('public.report_status.id'),
-               comment='Идентификатор статуса заявки'),
-        schema='public', comment='Список заявок'
-    )
+    op.create_table(ReportsModel.__tablename__, *ReportsModel.__schema__)
 
 
 def downgrade():
-    op.drop_table('reports')
-    op.drop_table('roles')
-    op.drop_table('critical_types')
-    op.drop_table('users')
-    op.drop_table('subdivision')
-    op.drop_table('positions')
-    op.drop_table('report_status')
+    op.drop_table(ReportsModel.__tablename__)
+    op.drop_table(CriticalTypesModel.__tablename__)
+    op.drop_table(UsersModel.__tablename__)
+    op.drop_table(RolesModel.__tablename__)
+    op.drop_table(PositionsModel.__tablename__)
+    op.drop_table(SubdivisionsModel.__tablename__)
+    op.drop_table(ReportStatusModel.__tablename__)
--- vAlert/database/models.py
+++ vAlert/database/models.py
@@ -1,10 +1,18 @@
-from sqlalchemy import Column, Text, Integer, Float
+from sqlalchemy import Column, Text, Float, Date, ForeignKey, Table, Boolean
+from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.orm import declarative_base
 
 Base = declarative_base()
 
 
-class AlembicVersionModel(Base):
+class ExtendTable(Table):
+    def __new__(cls, metadata):
+        self = super().__new__(cls, cls.__tablename__, metadata,
+                               *cls.__schema__)
+        return self
+
+
+class AlembicVersionModel(ExtendTable):
     """
     Текущая версия миграции
     """
@@ -13,23 +21,138 @@ class AlembicVersionModel(Base):
 
     version_num = Column('version_num', Text, primary_key=True, doc='Текущая версия миграции')
 
-
-class ReportModel(Base):
-    __tablename__ = 'report'
-    __table_args__ = {'schema': 'public'}
-
-    id = Column('id', Integer, primary_key=True, doc='Идентификатор')
-    title = Column('title', Text, doc='Название')
-    creator = Column('creator', Text, doc='Создатель')
+    __schema__ = [version_num]
 
 
-class CriticalTypeModel(Base):
+class CriticalTypesModel(ExtendTable):
     """
     Список типов критичности заявок
     """
-    __tablename__ = 'critical_type'
+    __tablename__ = 'critical_types'
     __table_args__ = {'schema': 'public'}
 
-    id = Column('id', Integer, primary_key=True, doc='Идентификатор')
-    name = Column('name', Text, doc='Название')
-    probability = Column('probability', Float, doc='Вероятность')
+    id = Column('id', UUID(as_uuid=True), primary_key=True, comment='Идентификатор')
+    name = Column('name', Text, unique=True, comment='Название типа критичности сообщения')
+    short_name = Column('short_name', Text, unique=True, comment='Короткое название для служебных нужд')
+    frequency = Column('frequency', Float, comment='Вероятность выдачи заяки')
+
+    __schema__ = [
+        id, name, short_name, frequency
+    ]
+
+
+class RolesModel(ExtendTable):
+    """
+    Список ролей
+    """
+    __tablename__ = 'roles'
+    __table_args__ = {'schema': 'public'}
+
+    id = Column('id', UUID(as_uuid=True), primary_key=True, comment='Идентификатор')
+    name = Column('name', Text, unique=True, comment='Название роли')
+
+    __schema__ = [
+        id, name
+    ]
+
+
+class SubdivisionsModel(ExtendTable):
+    """
+    Список подразделений
+    """
+    __tablename__ = 'subdivisions'
+    __table_args__ = {'schema': 'public'}
+
+    id = Column('id', UUID(as_uuid=True), primary_key=True, comment='Идентификатор')
+    name = Column('name', Text, unique=True, comment='Наименование подразделения')
+    parent_subdivision = Column('parent_subdivision', UUID(as_uuid=True),
+                                comment='Идентификатор родительского подразделения')
+
+    __schema__ = [
+        id, name, parent_subdivision
+    ]
+
+
+class PositionsModel(ExtendTable):
+    """
+    Список должностей
+    """
+    __tablename__ = 'positions'
+    __table_args__ = {'schema': 'public'}
+
+    id = Column('id', UUID(as_uuid=True), primary_key=True, comment='Идентификатор')
+    name = Column('name', Text, unique=True, comment='Наименование позиции')
+    subdivision = Column('subdivision', UUID(as_uuid=True), ForeignKey(SubdivisionsModel.id),
+                         comment='Идентификатор подразделения')
+    parent_position = Column('parent_position', UUID(as_uuid=True), comment='Идентификатор руководителя')
+    __schema__ = [
+        id, name, subdivision, parent_position
+    ]
+
+
+class UsersModel(ExtendTable):
+    """
+    Список пользователей
+    """
+
+    __tablename__ = 'users'
+    __table_args__ = {'schema': 'public'}
+
+    id = Column('id', UUID(as_uuid=True), primary_key=True, comment='Идентификатор')
+    first_name = Column('first_name', Text, comment='Имя')
+    last_name = Column('last_name', Text, comment='Фамилия')
+    patronymic = Column('patronymic', Text, comment='Отчество')
+    email = Column('email', Text, unique=True, comment='Адрес электронной почты')
+    phone = Column('phone', Text, unique=True, comment='Номер телефона')
+    password_hash = Column('password_hash', Text, comment='Хэш от пароля')
+    role = Column('role', UUID(as_uuid=True), ForeignKey(RolesModel.id), comment='Идентификатор роли')
+    disabled = Column('disabled', Boolean, comment='Активный пользователь')
+    position = Column('position', UUID(as_uuid=True), ForeignKey(PositionsModel.id),
+                      comment='Идентификатор должности')
+
+    __schema__ = [
+        id, first_name, last_name,
+        patronymic, email, phone,
+        password_hash, role, disabled,
+        position
+    ]
+
+
+class ReportStatusModel(ExtendTable):
+    """
+    Список статусов заявки
+    """
+    __tablename__ = 'report_status'
+    __table_args__ = {'schema': 'public'}
+
+    id = Column('id', UUID(as_uuid=True), primary_key=True, comment='Идентификатор')
+    name = Column('name', Text, unique=True, comment='Наименование')
+
+    __schema__ = [
+        id, name
+    ]
+
+
+class ReportsModel(ExtendTable):
+    """
+    Список заявок
+    """
+    __tablename__ = 'reports'
+    __table_args__ = {'schema': 'public'}
+
+    id = Column('id', UUID(as_uuid=True), primary_key=True, comment='Идентификатор')
+    title = Column('title', Text, comment='Название заявки')
+    description = Column('description', Text, comment='Описание заявки')
+    timestamp = Column('timestamp', Date, comment='Времянная метка')
+    reporter = Column('reporter', UUID(as_uuid=True), ForeignKey(UsersModel.id),
+                      comment='Идентификатор создателя заявки')
+    critical_types = Column('critical_types', UUID(as_uuid=True), ForeignKey(CriticalTypesModel.id),
+                            comment='Критичность')
+    resolver = Column('resolver', UUID(as_uuid=True), ForeignKey(UsersModel.id), comment='Идентификатор эксперта')
+    status = Column('status', UUID(as_uuid=True), ForeignKey(ReportStatusModel.id),
+                    comment='Идентификатор статуса заявки')
+
+    __schema__ = [
+        id, title, description, title, timestamp,
+        reporter, critical_types, resolver, status
+    ]
